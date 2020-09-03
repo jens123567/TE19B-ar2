ce4bbc02f94d9489b29ed01de06b4da37de6f233
Jens Malmqvist <jens.malmqvist@learnet.se>
2020-09-03 11:06:06 +0200

    övat på att redigera

---
+++ uppgift1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("rad 1"); // rad 1
+            Console.WriteLine("rad 2"); // rad 2
+            Console.WriteLine("rad 3, sista raden"); // rad 3
+        }
+    }
+}
---
+++ utmatning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TE19B_ar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
