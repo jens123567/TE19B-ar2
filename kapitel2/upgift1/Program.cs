09d61c5f5b5f4dea650bce347c9c699fe5e11ad5
Jens Malmqvist <jens.malmqvist@learnet.se>
2020-09-09 14:57:14 +0200

    övat på variabler inmatning flyttat in i kapitel

---
+++ kapitel2/upgift1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace upgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklarera ett nman för ariabel
+            string namn = "jens";
+            string hobby = "segla";
+
+
+            Console.WriteLine("Hej" + namn + hobby);
+        }
+    }
+}
---
+++ konsolen/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace konsolen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //change backgorund color of terminal
+            Console.BackgroundColor = ConsoleColor.Blue;
+            //change text color of terminal
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
