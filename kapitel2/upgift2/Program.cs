6f72691036dd1a5a5a919578813a88149ea20f95
Jens Malmqvist <jens.malmqvist@learnet.se>
2020-09-10 11:13:51 +0200

    nya grejor

---
+++ kapitel2/upgift2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace upgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklarera variabler
+            string name = "";
+            int age = 0;
+            int age2025 = 0;
+            double height = 0;
+            string message = "";
+
+            //läs in namn
+            Console.Write("vad heter du?");
+            name = Console.ReadLine();
+
+            Console.Write("hur lång är du");
+            height = double.Parse(Console.ReadLine());
+
+            Console.Write("hur gammal är du?");
+            age = int.Parse(Console.ReadLine());
+
+            //hur gammal är användaren 2020
+            age2025 = age + 5;
+
+            message = "hej " + name + " du är " + age2025 + " år gammal 2025 och är " + height + " meter lång";
+
+
+            Console.WriteLine(message);
+        }
+    }
+}
