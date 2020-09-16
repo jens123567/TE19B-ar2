5b617478e6094447a64b85dbf5c1d4057029129c
Jens Malmqvist <jens.malmqvist@learnet.se>
2020-09-16 15:02:14 +0200

    index

---
+++ kapitel2/2.6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double number1;
+            double number2;
+            double message;
+
+            Console.Write("SKRIV FÖRSTA TALET");
+            number1 = double.Parse(Console.ReadLine());
+
+            Console.Write("NU ÄR DET DAGS FÖR TAL TVÅ");
+            number2 = double.Parse(Console.ReadLine());
+
+            message = (number1 + number2) / 2;
+
+
+            Console.WriteLine(message);
+
+        }
+    }
+}
---
+++ kapitel2/2.7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en mening (utan punkt) som består av tre ord");
+            string mening = Console.ReadLine();
+            int spaceindex = mening.IndexOf(" ");
+            int spaceindex2 = mening.IndexOf(" ", spaceindex + 1);
+            string ord1 = mening.Substring(0, spaceindex);
+            string ord2 = mening.Substring(spaceindex, spaceindex2);
+            string ord3 = mening.Substring(spaceindex2 + 1);
+            string msg = ord3 + " " + ord2 + " " + ord1 + "\n" + ord2 + " " + spaceindex + " " + spaceindex2;
+
+
+            Console.WriteLine(msg);
+        }
+    }
+}
