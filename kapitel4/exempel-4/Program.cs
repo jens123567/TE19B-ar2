59b64bcd9f9f461232e0197097fb35380a8a0d02
Jens Malmqvist <jens.malmqvist@learnet.se>
2020-10-15 10:07:04 +0200

    comit

---
+++ kapitel4/exempel-4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace exempel_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //slumpa tal 1-6
+            Random tärning = new Random();
+            int slumptal = tärning.Next(1, 7);
+
+            // int gissning
+            int gissning = 0;
+
+            //loopa tills korrekt
+            while (gissning != slumptal)
+            {
+                //fråga vad man gissar
+                Console.WriteLine("gissa ett tal (1-6)");
+                // gissning = int.Parse(Console.ReadLine());
+
+
+                //bättre inmatning
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out gissning))
+                {
+                    Console.WriteLine("vad bra att du matade in ett tal");
+
+                    //kolla om rätt
+                    if (gissning != slumptal)
+                    {
+                        Console.WriteLine("fel svar testa igen");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("shame on you detta var inget tal");
+                }
+
+
+
+            }
+            //rätt svar
+            Console.WriteLine("rätt svar");
+
+        }
+    }
+}
---
+++ kapitel4/frågesport/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace frågesport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            int score = 0;
+            string choice = "";
+            string yesno = "";
+
+            Console.WriteLine("vad heter du?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hej " + name);
+
+            while (yesno != "nej")
+            {
+                while (choice != "2")
+                {
+                    Console.WriteLine("Vad heter huvudstaden i tyskland?");
+                    Console.WriteLine("1. Stockholm");
+                    Console.WriteLine("2. Berlin");
+                    Console.WriteLine("3. Belgrad");
+
+                    choice = Console.ReadLine();
+
+                    if (choice == "2")
+                    {
+                        Console.WriteLine("rätt svar");
+                        score++;
+                        Console.WriteLine("du har " + score + " poäng");
+                    }
+                    else
+                    {
+                        Console.WriteLine("fel, testa igen");
+                        score--;
+                    }
+                }
+
+                Console.WriteLine("Vill " + name + " spela igen? (ja/nej)");
+                yesno = Console.ReadLine();
+
+                if (yesno != "ja")
+                {
+                    Console.WriteLine("tråkigt hoppas vi ses snart igen");
+                }
+                else
+                {
+                    choice = "0";
+                    score = 0;
+                }
+            }
+
+        }
+    }
+}
