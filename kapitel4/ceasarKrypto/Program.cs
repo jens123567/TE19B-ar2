7300f97e81346363d79bf9e856d44be1f0616f90
Jens Malmqvist <jens.malmqvist@learnet.se>
2020-11-04 15:58:19 +0100

    comit

--- kapitel4/ceasarKrypto/Program.cs
+++ kapitel4/ceasarKrypto/Program.cs
@@ -6,7 +6,55 @@ namespace ceasarKrypto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ange en text
+            Console.Write("ange en text:");
+            string text = Console.ReadLine();
+
+            //loopa igenom inmatad text bokstav för bokstav
+            int textLängd = text.Length;
+            Console.WriteLine($"Texten är {textLängd} tecken lång");
+
+            //be användareb om nyckel
+            Console.WriteLine("ange en nyckel (1-9): ");
+            string nyckelString = Console.ReadLine();
+
+            //säkerställa att vi får in ett tal
+            int nyckel = 0;
+
+            while (!int.TryParse(nyckelString, out nyckel))
+
+            {
+                Console.WriteLine("du gjorde fel! ange en nyckel (1-9): ");
+                nyckelString = Console.ReadLine();
+            }
+
+            //loopa igenom bokstav för bokstav
+            string krypteradText = "";
+            for (int i = 0; i < textLängd; i++)
+            {
+                /*Console.WriteLine($"loop nr {i}");*/
+
+                //plocka ut bokstav på postition i
+                char bokstav = text[i];
+                Console.WriteLine($"Bokstaven på position {i} är {bokstav}");
+
+                //ASCII-värdet för ett tecken
+                int ascii = (int)bokstav;
+                Console.WriteLine($"bokstaven {bokstav} har ACII-värdet {ascii}");
+
+                //ceasar kryptering
+                ascii += nyckel;
+
+                //plock ut motsvarande tecken enligt ascii
+                char krypteradBokstav = (char)ascii;
+                Console.WriteLine($"bokstaven {bokstav} krypteras till {krypteradBokstav} ");
+
+
+                //samla ihop bokstäver
+                krypteradText += krypteradBokstav.ToString();
+            }
+            //skriv ut krypterade texten
+            Console.WriteLine($"texten {text} har krypterats till {krypteradText}");
         }
     }
 }
