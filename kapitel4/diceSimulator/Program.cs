04aae1153fba47c04458ec4200c1fb0cf05de2c7
Jens Malmqvist <jens.malmqvist@learnet.se>
2020-11-11 15:02:14 +0100

    comit

---
+++ kapitel4/diceSimulator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace diceSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numbDice;
+            int sideDice;
+
+
+            Console.WriteLine("Hur många tärningar vill du slå?");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out numbDice))
+            {
+
+                Console.WriteLine("Hur många sidor ska tärningarna ha?");
+                string input1 = Console.ReadLine();
+
+                if (int.TryParse(input1, out sideDice))
+                {
+                    for (int i = 0; i < numbDice; i++)
+                    {
+
+
+
+                        Random wheel = new Random();
+                        int slumptal = wheel.Next(1, sideDice);
+
+                        //int dice;
+                        //dice++;
+
+                        Console.WriteLine("Tärning hade resultatet " + slumptal);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ingen siffra");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("det var ingen siffra");
+            }
+        }
+    }
+}
