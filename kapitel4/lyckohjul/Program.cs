3dcaa6f536c070ccc6d4ede34507707535f777d8
Jens Malmqvist <jens.malmqvist@learnet.se>
2020-11-11 14:32:15 +0100

    comit

---
+++ kapitel4/lyckohjul/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace lyckohjul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string yesno = "";
+
+            while (yesno != "no")
+            {
+                int guess;
+
+                Random wheel = new Random();
+                int slumptal = wheel.Next(1, 11);
+
+                Console.WriteLine("gissa tal 1-10");
+                string input = Console.ReadLine();
+
+
+                if (int.TryParse(input, out guess))
+                {
+                    if (guess == slumptal)
+                    {
+                        Console.WriteLine("Du hade samma gissning som slumptalet vilket var " + slumptal);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Rätt svar var " + slumptal + " alltså så var ditt svar fel");
+                    }
+
+                    Console.WriteLine("spela igen yes/no");
+                    yesno = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("du måste skriva en siffra");
+                }
+
+
+            }
+
+
+        }
+    }
+}
