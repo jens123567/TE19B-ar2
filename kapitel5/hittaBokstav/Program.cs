7b29f74047ab8bbe0910f4f6ffb3a5e4ef70f29d
Jens Malmqvist <jens.malmqvist@learnet.se>
2020-11-25 14:58:15 +0100

    morse Tolk

---
+++ kapitel5/hittaBokstav/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace hittaBokstav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ange en bokstav
+            Console.WriteLine("ange en bokstav");
+            string bokstav = Console.ReadLine().ToUpper();
+
+            //skapa en samling för alfabetet
+            string alfabetet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ ";
+
+            //hitta bokstavens plats i alfabetet
+            int index = alfabetet.IndexOf(bokstav);
+
+            //Vart fanns bokstaven
+            Console.WriteLine($"{bokstav} finns på position {index}");
+
+            //skapa en samling för morse koden
+            string[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", ".--", "...-", ".--", "-..-", "-.--", "--..", ".--.-", ".-.-", "---.", " " };
+
+            //skriv ut morse koden för bokstaven
+
+
+            Console.WriteLine($"{bokstav} är {morse[index]} i morse");
+        }
+    }
+}
---
+++ kapitel5/morseTolk/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace morseTolk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Berätta om programet
+            //be om ett meddelande
+            Console.WriteLine("Programmet översätter din text till morse, mata in ett ord");
+            string meddelande = Console.ReadLine().ToUpper();
+
+            string morseMeddelande = "";
+
+            //skapa en samling för alfabetet
+            string alfabetet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ ";
+
+            //skapa en samling för morse koden
+            string[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", ".--", "...-", ".--", "-..-", "-.--", "--..", ".--.-", ".-.-", "---.", " /" };
+
+            //loopa igenom meddelandet 
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                //skriv ut varje tecken
+                string bokstav = meddelande[i].ToString();
+
+                //hitta bokstavens plats i alfabetet
+                int index = alfabetet.IndexOf(bokstav);
+
+                //skriv ut morsekoden för bokstaven
+                Console.WriteLine($"{bokstav} = {morse[index]} i morse");
+                morseMeddelande += morse[index] + " ".ToString();
+            }
+            Console.WriteLine($"{meddelande} är {morseMeddelande} i morse");
+
+        }
+    }
+}
