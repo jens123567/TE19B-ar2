dbf754d301aec8294099e2e47df00b8baaaa15aa
jens123567 <jens.malmqvist03@gmail.com>
2021-04-14 14:44:55 +0200

    comit

--- kapitel6/6.23/Program.cs
+++ kapitel6/6.23/Program.cs
@@ -6,7 +6,42 @@ namespace _6._23
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("skriv ett meddelande");
+            string meddelande = Console.ReadLine();
+
+            Console.WriteLine(VarannanStor(meddelande));
+            Console.WriteLine(GörFörstaBokstavStor(meddelande));
+        }
+
+        static string VarannanStor(string texten)
+        {
+
+            texten = texten.ToLower();
+            //Dela upp texten vid mellanslagen
+            string[] orden = texten.Split(' ');
+
+            //loopa igenom texten och gör vart annat ord stort
+            for (int i = 0; i < orden.Length; i += 2)
+            {
+                orden[i] = orden[i].ToUpper();
+            }
+
+            return string.Join(" ", orden);
+        }
+
+        static string GörFörstaBokstavStor(string texten)
+        {
+            string[] orden = texten.Split(' ');
+
+            for (int i = 0; i < orden.Length; i++)
+            {
+                string ord = orden[i];
+                string firstLetter = ord.Substring(0, 1).ToUpper();
+                string restOfWord = ord.Substring(1).ToLower();
+                orden[i] = firstLetter + restOfWord;
+            }
+
+            return string.Join(" ", orden);
         }
     }
 }
